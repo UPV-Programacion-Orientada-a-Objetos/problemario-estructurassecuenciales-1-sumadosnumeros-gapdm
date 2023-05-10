1bcc29483be19719a7fee6806522ca2efed00572
gapdm <2130171@upv.edu.mx>
2023-05-10 11:52:53 -0600

    Funciona

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main {
-
     public static void main(String[] args) {
-
+	SumaDosNumeros Suma = new SumaDosNumeros();
+	int n1,n2;
+	n1 = Integer.parseInt(args[0]);
+	n2 = Integer.parseInt(args[1]);
+	System.out.println(Suma.suma(n1,n2));
     }
 }
